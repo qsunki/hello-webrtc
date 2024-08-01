e4efa8671925d15ea9a8d962c0ac661467e4969b
qsunki <ajsthfldu@outlook.com>
2024-08-01 22:59:20 +0900

    전체 수정

--- src/main/java/com/example/webrtc/WebSocketConfig.java
+++ src/main/java/com/example/webrtc/WebSocketConfig.java
@@ -9,10 +9,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
     }
 
     @Override
